e62075e9ddcb2d7cd879306ceea4aa91d25e83fd
suicidal-ghost <49636009+suicidal-ghost@users.noreply.github.com>
2019-04-15 04:31:54 -0400

    Add files via upload

---
+++ ChildNode.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System.Collections.Generic;
+using System;
+
+public class ChildNode : Spatial
+{
+    //emits signal when this node is ready so that parent node can connect to it
+    [Signal]
+    public delegate void ChildReady(Spatial s);
+
+    //for sending each list element 
+    [Signal]
+    public delegate void SendListElement(uint u);
+
+    Random random;
+    List<uint> childList = new List<uint>();
+
+    public override void _Ready()
+    {
+        Spatial ParentNode = GetParent<Spatial>();
+        this.Connect(nameof(ChildReady), ParentNode, "_on_Child_Register");
+        EmitSignal(nameof(ChildReady), this);
+        random = new Random();
+        this.Connect(nameof(SendListElement), ParentNode, "_on_ListElement_Emit");
+    }
+
+    // can uncomment this and comment out the foreach statement in _on_List_Request() for a control test that will only generate and add an element to the list every frame without emmitting it.
+    // public override void _Process(float delta) {
+    //     _on_List_Request();   
+    // }
+
+    //Generates a maximum of a 32 bit uint (can be less depending on RNG) end emits all the list elements to the parent node
+    public void _on_List_Request() {
+        uint thirtybits = (uint)random.Next(1 << 30);
+        uint twobits = (uint)random.Next(1 << 2);
+        uint finaluint = (thirtybits << 2) | twobits;
+        childList.Add(finaluint);
+        foreach(uint u in childList) {
+            EmitSignal(nameof(SendListElement), u);
+        }
+    }
+
+    //this can be used to get childlist count when the timer runs out buy uncommenting the GetChild(0).QueueFree(); line in CheckTime() on the Parent Node. This will cause this program to crash however. 
+    private void _on_ChildNode_tree_exiting() {
+       GD.Print("ChildListCount: " + childList.Count);
+    }
+
+
+
+}
+
+
+
+
---
+++ ParetnNode.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System.Collections.Generic;
+using System;
+
+public class ParetnNode : Spatial
+{
+    float DeltaTime;
+    Spatial ChildNode;
+    List<uint> uintList = new List<uint>();
+    int TestTime = 10;
+
+    //emit to child node to request the list be sent one element at a time
+    [Signal]
+    public delegate void RequestList();
+
+    public override void _Ready()
+    {
+        DeltaTime = 0;
+    }
+
+    public override void _Process(float delta) {
+        DeltaTime += delta;
+        CheckTime();
+        uintList = new List<uint>();       //reinitialize list before requesting it
+        EmitSignal(nameof(RequestList));
+    }
+
+    //cannot connect to child node until it is ready
+    public void _on_Child_Register (Spatial s) {
+        ChildNode = s;
+        this.Connect(nameof(RequestList), ChildNode, "_on_List_Request");
+    }
+
+    //can uncomment the two lines below to have the child node print the list count and close the application when DeltaTime > TestTime. However, this will cause Godot to crash after execution.
+    public void CheckTime() {
+        if (DeltaTime >= (float)TestTime) {
+            //GetChild(0).QueueFree();
+            GD.Print("Elapsed Time: " + DeltaTime);
+            GD.Print("List Count: " + uintList.Count);
+            //GetTree().Quit();
+        }
+    }
+
+    //recieves list element and adds it the list on this node
+    public void _on_ListElement_Emit(uint u) {       
+        uintList.Add(u);
+    }
+
+}
